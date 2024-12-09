6aa6de0d7c4802dcdc734606e85248b4d3f978af
leekhqwet <85793515+leekhqwet@users.noreply.github.com>
2024-12-09 14:53:26 +0900

    Update flask_app_test.py

--- flask_app_test.py
+++ flask_app_test.py
@@ -1,11 +1,7 @@
+# 사용자 입력 받기
+num1 = int(input("첫 번째 숫자를 입력하세요: "))
+num2 = int(input("두 번째 숫자를 입력하세요: "))
 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
+# 합 계산 및 출력
+result = num1 + num2
+print("두 숫자의 합은:", result)
